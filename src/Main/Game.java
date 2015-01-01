783143f6dd1abd6f28ed792340f72a22a8fc390f
craftbro <yntegamer@gmail.com>
2015-01-01 11:00:51 +0100

    FANCEY

--- src/Main/Game.java
+++ src/Main/Game.java
@@ -1,5 +1,21 @@
 package Main;
 
+import javax.swing.JFrame;
+
 public class Game {
 
+	
+	public static void main(String[] args){
+		JFrame frame = new JFrame("TicTacToe");
+		GamePanel panel = new GamePanel();
+		
+		frame.setResizable(false);
+		frame.setPreferredSize(panel.SIZE);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.setContentPane(panel);
+		
+		frame.setVisible(true);
+	}
+	
 }
---
+++ src/Main/GamePanel.java
@@ -0,0 +1,11 @@
+package Main;
+
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel{
+
+	public static final Dimension SIZE = new Dimension(800, 600);
+	
+}
